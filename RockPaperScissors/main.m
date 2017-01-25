3cd277983ed70d01d53ff4ef35cf00b9e54f2252
Jena Grafton <JenaLGrafton@gmail.com>
2017-01-25 11:11:01 -0600

    Initial Commit

---
+++ RockPaperScissors/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RockPaperScissors
+//
+//  Created by Jena Grafton on 1/25/17.
+//  Copyright © 2017 Bella Voce Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
